1fa2c048ac480619608b9931e1714358c73eb540
khyati bhardwaj <khyatibhardwaj0103@gmail.com>
2025-09-25 16:06:42 +0530

    DAY_09_B_GRADE_PERCENT.C

---
+++ DAY_09_B_GRADE_PERCENT.C
@@ -0,0 +1,56 @@
+Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F.
+
+/*
+Sample Test Cases:
+Input 1:
+95
+Output 1:
+Grade A
+
+Input 2:
+82
+Output 2:
+Grade B
+
+Input 3:
+68
+Output 3:
+Grade D
+
+Input 4:
+50
+Output 4:
+Grade F
+
+*/
+
+  //solution
+#include <stdio.h>
+
+int main() {
+    int percent;
+
+    // Read input percentage
+    if (scanf("%d", &percent) != 1) {
+        return 1;  // error in input
+    }
+
+    // Check valid range
+    if (percent < 0 || percent > 100) {
+        printf("Invalid input\n");
+    }
+    else if (percent >= 90) {
+        printf("Grade A\n");
+    }
+    else if (percent >= 80) {
+        printf("Grade B\n");
+    }
+    else if (percent >= 70) {
+        printf("Grade C\n");
+    }
+    else if (percent >= 60) {
